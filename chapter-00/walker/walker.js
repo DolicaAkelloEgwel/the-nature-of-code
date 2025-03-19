299bfa7e26335ea52dcbf093f96dd06fb9024275
Dolica Akello-Egwel <d.egwel@gmail.com>
2025-03-19 22:13:02 +0000

    using random instead

--- chapter-00/walker/walker.js
+++ chapter-00/walker/walker.js
@@ -8,20 +8,12 @@ class Walker {
     point(this.x, this.y);
   }
   step() {
-    let choice = floor(random(4));
-    switch(choice) {
-      case 0:
-        this.x++;
-        break;
-      case 1:
-        this.x--;
-        break;
-      case 2:
-        this.y++;
-        break;
-      case 3:
-        this.y--;
-        break;
+    let xstep = random(-1, 1);
+    let ystep = random(-1, 1);
+    
+    this.x += xstep;
+    this.y += ystep;
+    
     }
   }
 }

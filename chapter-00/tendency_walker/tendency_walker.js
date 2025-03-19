4bfb2001dc90b60a3efc0bfdef6734a3356b6c01
Dolica Akello-Egwel <d.egwel@gmail.com>
2025-03-19 22:35:39 +0000

    exercise 0.1

---
+++ chapter-00/tendency_walker/tendency_walker.js
@@ -0,0 +1,26 @@
+class Walker {
+  constructor() {
+    this.x = width / 2;
+    this.y = height / 2;
+  }
+  show() {
+    stroke(0);
+    point(this.x, this.y);
+  }
+  step() {
+    let xstep = random(-1, 2);
+    let ystep = random(-2, 1);
+    
+    this.x += xstep;
+    this.y += ystep;
+  }
+}
+function setup() {
+  createCanvas(640, 640);
+  walker = new Walker();
+  background(255);
+}
+function draw() {
+  walker.step();
+  walker.show();
+}

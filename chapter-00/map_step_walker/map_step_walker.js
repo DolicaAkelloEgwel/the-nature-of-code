3dfb8b2898e3f47298029d28ab5385a5c66031b9
Dolica Akello-Egwel <d.egwel@gmail.com>
2025-03-28 09:56:42 +0000

    map step walker

--- chapter-00/map_step_walker/map_step_walker.js
+++ chapter-00/map_step_walker/map_step_walker.js
@@ -5,23 +5,28 @@ class Walker {
     this.t = 0;
     this.x = width / 2;
     this.y = height / 2;
+    this.dx = 0;
+    this.dy = 0;
   }
   show() {
     fill(255, 255, 0);
     line(this.prev_x, this.prev_y, this.x, this.y);
   }
   step() {
-    let radius = map(noise(this.t), 0, 1, 0, MAX_STEP);
-    let angle = random(TWO_PI);
-    
-    this.prev_x = this.x;
-    this.prev_y = this.y;
-    
-    this.x += radius * cos(angle);
-    this.y += radius * sin(angle);
-    
-    this.t += 0.01;
-    
+
+      let radius = map(noise(this.t), 0, 1, 0, MAX_STEP);
+      let angle = random(TWO_PI);
+
+      this.dx = radius * cos(angle);
+      this.dy = radius * sin(angle);
+
+      this.prev_x = this.x;
+      this.prev_y = this.y;
+
+      this.x += this.dx;
+      this.y += this.dy;
+
+      this.t += 0.01;
     }
   }
 function setup() {

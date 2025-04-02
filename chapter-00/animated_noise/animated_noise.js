da1d26c7f41798dae93cd25b5c20106ecccdcd73
Dolica Akello-Egwel <d.egwel@gmail.com>
2025-04-02 21:44:04 +0100

    animated noise

---
+++ chapter-00/animated_noise/animated_noise.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(200, 200);
+}
+
+
+function draw() {
+  let xstep = 0.01;
+  let ystep = 0.01;
+  let zstep = 0.02;
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      let bright = map(noise(x * xstep, y * ystep, frameCount * zstep), 0, 1, 0, 255);
+      stroke(bright);
+      point(x, y);
+    }
+  }
+}

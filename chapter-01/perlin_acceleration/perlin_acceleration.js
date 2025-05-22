b9407346b499fbb8d9cdf17f86d483dbf0a9713e
DolicaAkelloEgwel <d.egwel@gmail.com>
2025-05-22 17:24:35 +0100

    perlin acceleration

---
+++ chapter-01/perlin_acceleration/perlin_acceleration.js
@@ -0,0 +1,52 @@
+let mover;
+
+class Mover {
+  constructor() {
+    this.position = createVector(width / 2, height / 2);
+    this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0,0);
+    this.topSpeed = 5;
+    this.t = 0;
+  }
+  update() {
+    this.acceleration = createVector(noise(this.t),noise(this.t + 5));
+    this.acceleration.mult(5);
+    this.acceleration.sub(2.5,2.5);
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.topSpeed);
+    this.position.add(this.velocity);
+    this.t += 0.01;
+  }
+  show() {
+    stroke(0);
+    strokeWeight(2);
+    fill(127);
+    circle(this.position.x, this.position.y, 48);
+  }
+  checkEdges() {
+    if (this.position.x > width) {
+      this.position.x = 0;
+    }
+    else if (this.position.x < 0) {
+      this.position.x = width;
+    }
+    if (this.position.y > height) {
+      this.position.y = 0;
+    }
+    else if (this.position.y < 0) {
+      this.position.y = height;
+    }
+  }
+}
+
+function setup() {
+  createCanvas(800, 800);
+  mover = new Mover();
+}
+
+function draw() {
+  background(255);
+  mover.update();
+  mover.checkEdges();
+  mover.show();
+}
